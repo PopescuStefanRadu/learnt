7e4f10bcd02885e170b4465d43d9761ca71c27c8
Popescu Stefan Radu <popescustefanradu@gmail.com>
2024-08-05 17:42:40 +0300

    update CV

--- cv/build.sh
+++ cv/build.sh
@@ -7,6 +7,7 @@ MAIL=$3
 if [ -z "$NAME" ] || [ -z $PHONE ] || [ -z $MAIL ]
 then
   echo "One or more parameters are missing. Parameter order: name phone mail"
+  echo 'Example usage: ./build.sh "John Doe" "+40721212121" "mail@example.com"'
   exit 1
 fi
 
